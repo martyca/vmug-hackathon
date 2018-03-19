ae8206c574a5850f1baf1d3fde224a7764ea3669
Chris <croeleveld@gmail.com>
2018-03-19 16:42:30 +0100

    gobot first commit

---
+++ gobot/deploy_minecraft.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export BOSH_CLIENT_SECRET=d8hcm8gj6td59as3rliu
+export BOSH_CLIENT=admin
+bosh -e hackaton -d minecraft1 deploy ./minecraft.yml -v deployment-name=minecraft1 --vars-store ./mincraft1-creds.yml
+
+exit 0
