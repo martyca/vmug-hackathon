2ab28d0f7e010eca4861fee35880966404da8a0b
Sander Harrewijnen <sharrewijnen@itq.nl>
2018-03-19 15:35:06 +0100

    Updated visuals and message cap.

--- steve-ui/wwwroot/app.js
+++ steve-ui/wwwroot/app.js
@@ -3,14 +3,22 @@ var vm = (function () {
     self.connection = new signalR.HubConnection('/messagehub');
 
     self.messages = ko.observableArray();
+    self.isTalking = ko.observable(false);
 
     // Constructor
 
     self.connection.on("send", data => {
         self.messages.push(data);
+
+        self.isTalking(true);
+        setTimeout(() => { self.isTalking(false); }, 750);
+
+        if (self.messages().length > 10) {
+            self.messages.shift();
+        }
     });
 
-    self.connection.start() //.then(() => connection.invoke('send', 'Hello'));
+    self.connection.start();
 })();
 
 ko.applyBindings(vm, document.getElementById("app"));
