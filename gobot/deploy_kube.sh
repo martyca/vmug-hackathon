adfcfb3bea0ad27c1dd4aee252babe27364b1d6b
Chris <croeleveld@gmail.com>
2018-03-19 19:56:08 +0100

    enable pks

--- gobot/deploy_kube.sh
+++ gobot/deploy_kube.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-#pks login -a https://uaa.pks.pcf.lab.local -u admin -k -p UjfQ_II0P--mGynVk64ssXyUykA3oGeB
-#pks create-cluster vmug --external-hostname vmug.pks.pcf.lab.local --plan small
-
-sleep 2
+pks login -a https://uaa.pks.pcf.lab.local -u admin -k -p UjfQ_II0P--mGynVk64ssXyUykA3oGeB
+pks create-cluster vmug --external-hostname vmug.pks.pcf.lab.local --plan small
 
 exit 0
