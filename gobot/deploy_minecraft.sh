cdcb64a143e5e7ff7698d9b9198e21d66064ae50
Chris <croeleveld@gmail.com>
2018-03-19 17:51:27 +0100

    del ip from deploy script

--- gobot/deploy_minecraft.sh
+++ gobot/deploy_minecraft.sh
@@ -4,6 +4,4 @@ export BOSH_CLIENT_SECRET=d8hcm8gj6td59as3rliu
 export BOSH_CLIENT=admin
 bosh -e hackaton -d minecraft1 deploy /home/ubuntu/hackaton/minecraft.yml -v deployment-name=minecraft1 --vars-store /home/ubuntu/hackaton/minecraft1-creds.yml -n
 
-bosh -e hackaton -d minecraft1 vms
-
 exit 0
