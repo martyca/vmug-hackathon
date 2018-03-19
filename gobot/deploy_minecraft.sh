637518e9e0d79062cfa57b5d1dc83eba1ecfcfc2
Chris <croeleveld@gmail.com>
2018-03-19 17:09:02 +0100

    list VMs after deploy.

--- gobot/deploy_minecraft.sh
+++ gobot/deploy_minecraft.sh
@@ -4,4 +4,6 @@ export BOSH_CLIENT_SECRET=d8hcm8gj6td59as3rliu
 export BOSH_CLIENT=admin
 bosh -e hackaton -d minecraft1 deploy ./minecraft.yml -v deployment-name=minecraft1 --vars-store ./mincraft1-creds.yml
 
+bosh -e hackaton -d minecraft1 vms
+
 exit 0
